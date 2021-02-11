569b4acd29d3189785791cb5fcd9b20cdfba04a2
Himangshu De <dehimangshu2020@gmail.com>
2021-02-11 14:47:17 +0530

    Added Model QPaper 11 Sol.py and modified it with latest updates. 1447_11022021

---
+++ Model QPaper 11 Sol.py
@@ -0,0 +1,39 @@
+"""Chapter STRING"""
+"""Q1 Palindrome Test"""
+
+# user_string = input("Enter string: ")
+
+# list_str = user_string.split(" ")
+
+# length = len(list_str)
+
+# joiner = ""
+
+# for i in range(length,-1):
+#     joiner = list_str[i] + list_str[i]
+    
+# rev_joiner = joiner[::-1]
+# if rev_joiner.casefold() == joiner.casefold():
+#     print(f"\"{user_string}\" is a Palindrome String")
+# else:
+#     print(f"{user_string} is not a Palindrome String")
+
+
+"""Q2 Display even position elements from a user input."""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
